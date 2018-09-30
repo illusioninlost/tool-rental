613ceb671b2d1b73f2c8fb5657f01a68548f7c41
illusioninlost <rchang9999@yahoo.com>
2018-09-29 17:43:59 -0700

    create store

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+import {createStore, applyMiddleware} from 'redux';
 
 
-ReactDOM.render(<Provider>
+const store = createStore(rootReducer)
+
+ReactDOM.render(
+    <Provider>
     <App />
     </Provider>
-    , document.getElementById('root'));
+    , document.getElementById('root')
+);
+
 registerServiceWorker();
