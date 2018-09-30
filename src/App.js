2c2f68fdd9430d588ac03092d62ba6d5035ac9df
illusioninlost <rchang9999@yahoo.com>
2018-09-29 18:03:18 -0700

    add app imports

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
+import {Navbar} from 'react-bootstrap';
+import {connect} from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
+import {bindActionCreators} from 'redux';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          Edited
-        </p>
+      
       </div>
     );
   }
