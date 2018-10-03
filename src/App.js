cd0128bac338c5abb11c14714e71ae01fce21aa5
illusioninlost <rchang9999@yahoo.com>
2018-10-03 07:31:20 -0700

    motto

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import './App.css';
 import { bindActionCreators } from 'redux';
-
+import Head from './components/head';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
-        <Navbar>
-        <Navbar.Header>
-        <Navbar.Brand>
-          <a href="#home">Tool Rental</a>
-        </Navbar.Brand>
-        </Navbar.Header>
-        <Nav>
-        <NavItem eventKey={1} href="#">
-          Link
-        </NavItem>
-        <NavItem eventKey={2} href="#">
-          Link
-        </NavItem>
-        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-        <MenuItem eventKey={3.1}>Action</MenuItem>
-        <MenuItem eventKey={3.2}>Another action</MenuItem>
-        <MenuItem eventKey={3.3}>Something else here</MenuItem>
-        <MenuItem divider />
-        <MenuItem eventKey={3.4}>Separated link</MenuItem>
-        </NavDropdown>
-        </Nav>
-        </Navbar>;
+        <Head/>
        
-        <div>Tools to rent. Save time and money. &#128296;</div>
+       
+        <div className="Motto">Tools to rent. Save time and money. &#128296;</div>
         <div className="cart">
         Cart side bar items should pop out after clicking 
         </div>
