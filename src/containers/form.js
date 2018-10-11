6573c2848ed9a81b0e3a8c08a471557a50552ed1
illusioninlost <rchang9999@yahoo.com>
2018-10-11 12:15:58 -0700

    add form data and tool name to alt pics

--- src/containers/form.js
+++ src/containers/form.js
@@ -4,7 +4,12 @@ class ToolForm extends Component{
     
     render(){
         return(<div>
-            
+            <form>
+                <input type="text" name="toolName"/>
+                <input type="text" name="toolDescription"/>
+                <input type="number" name="toolPrice"/>
+                <input type="submit" value="Submit">
+            </form>
         </div>)
     }
 }
