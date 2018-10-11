9c13baa5682848a5a5e7b1493c3b10b0f0cccf63
illusioninlost <rchang9999@yahoo.com>
2018-10-11 07:24:26 -0700

    change format for tools

--- src/containers/tools.js
+++ src/containers/tools.js
@@ -1,63 +1,65 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import {fetchTools} from '../actions/toolActions'
+import { fetchTools } from '../actions/toolActions'
 // import Item from '../components/item'
-import {Grid, Row, Col} from 'react-bootstrap'
+import { Grid, Row, Col } from 'react-bootstrap'
 
-class Tools extends Component { 
-   
+class Tools extends Component {
 
-    componentDidMount(){
+
+    componentDidMount() {
         this.props.fetchTools()
     }
-    
+
     render() {
-       
-        return(
+
+        return (
             <div>
                 <Grid>
-  
 
-  <Row className="show-grid">
-  {this.props.myTools.map((t)=> {
-            
-            return(
-               <Col xs={6} md={4}>
-               <img className="toolpic" src={t.url} />
-               </Col>
-               
-            )
-        })}
-       
-  </Row>
 
-</Grid>
-       
-      
-           
-          
+                    <Row className="show-grid">
+                        {this.props.myTools.map((t) => {
+
+                            return (
+                                <Col xs={6} md={4}>
+                                    <img className="toolpic" src={t.url} />
+                                    <p>{t.description}</p>
+                                    <p>&#36;{t.price}&#47;hour</p>
+                                </Col>
+
+                            )
+                        })}
+
+                    </Row>
+
+                </Grid>
+
+
+
+
 
             </div>
         )
     }
-    
-   
-    
+
+
+
 
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({
-    fetchTools: fetchTools,
-  }, dispatch)
+    return bindActionCreators({
+        fetchTools: fetchTools,
+    }, dispatch)
 }
 
 
 const mapStateToProps = state => {
 
-  return { myTools: state.tools.tools};
+    return { myTools: state.tools.tools };
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(Tools)
+export default connect(mapStateToProps, mapDispatchToProps)(Tools)
 
