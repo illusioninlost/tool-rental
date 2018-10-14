cb660921d4bed751d6f7d5bbe72dd18b3d60decc
illusioninlost <rchang9999@yahoo.com>
2018-10-14 12:48:43 -0700

    fix emoji error

--- src/components/motto.js
+++ src/components/motto.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Motto = () => {
     return(
-    <div className="Motto">Tools to rent. Save time and money.<span> &#128296; </span><hr/></div>
+    <div className="Motto">Tools to rent. Save time and money.<span role="img" aria-label="Hammer"> &#128296; </span><hr/></div>
     
     )
 }
