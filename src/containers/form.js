5cc50d595f946c26f3e401b04df54c4dc8b2c7a0
illusioninlost <rchang9999@yahoo.com>
2018-10-15 11:13:47 -0700

    fix form on moz

--- src/containers/form.js
+++ src/containers/form.js
@@ -3,8 +3,15 @@ import React, {Component} from 'react';
 
 class toolForm extends Component{
 
+    state = {
+        toolName: '',
+        toolDescription: '',
+        toolPrice: 0,
+        toolImage: ''
+    }
+
     handleOnChange = (event) => {
-        console.log("that one");
+        this.setState({[event.target.name]: event.target})
     }
     
     handleOnSubmit = (event) => {
@@ -16,26 +23,30 @@ class toolForm extends Component{
     render(){
 
         return(<div>
+           
+            <form className="ui form" onSubmit={this.handleOnSubmit}>
             <h1> Place Tool Ad </h1>
-            <form onSubmit={this.handleOnSubmit}>
-                <div>
-                <label>Name:</label>
-                <input type="text" name="toolName"  onChange={this.handleOnChange}/>
+                <div className="field">
+                <label htmlFor="toolName">Name:</label>
+                <input type="text" name="toolName" value={this.state.toolName.value} onChange={this.handleOnChange}/>
                 </div>
-                <div>
-                <label>Description:</label>
-                <input type="text" name="toolDescription"  onChange={this.handleOnChange}/>
+                <div className="field">
+                <label htmlFor="toolDescription">Description:</label>
+                <input type="text" name="toolDescription" value={this.state.toolDescription.value} onChange={this.handleOnChange}/>
                 </div>
-                <div>
-                <label> Price per hour:</label>
-                <input type="number" step="0.01" name="toolPrice" onChange={this.handleOnChange}/>
+                <div className="field">
+                <label htmlFor="toolPrice"> Price per hour:</label>
+                <input type="number" step="0.01" name="toolPrice" value={this.state.toolPrice.value} onChange={this.handleOnChange}/>
                 </div>
-                <div>
-                <label>URL:</label>
-                <input type="text" name="toolImage"  onChange={this.handleOnChange}/>
+                <div className="field">
+                <label htmlFor="toolImage">URL:</label>
+                <input type="text" name="toolImage"  value={this.state.toolImage.value} onChange={this.handleOnChange}/>
                 </div>
-                <div>
-                <input type="submit" value="Submit" />
+                <div className="field">
+                    <img src={this.state.toolImage.value} alt="toolImage" className="ui small bordered image"/>
+                </div>
+                <div className="field">
+                <button className="ui primary button"> Save </button>
                 </div>
             </form>
             </div>
