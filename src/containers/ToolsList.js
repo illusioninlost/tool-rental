06f061bdf22da480f6daae73314bef22d4ff8356
illusioninlost <rchang9999@yahoo.com>
2018-10-15 09:29:28 -0700

    ToolsList component

--- src/containers/ToolsList.js
+++ src/containers/ToolsList.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 export default function ToolsList({ tools }){
 
+    const emptyMessage = (
+        <p> There are no tools to rent at this time. Please try again or post one. </p>
+    )
+
+    const toolsList = (
+        <p>tools list</p>
+    )
     return (
         <div>
-
+            {toolsList === 0 ? emptyMessage: toolsList}
         </div>
     );
 }
