e290b5c7800f5133627b4e088f577d81022cce68
illusioninlost <rchang9999@yahoo.com>
2018-10-16 23:36:39 -0700

    space issue

--- src/containers/ToolCard.js
+++ src/containers/ToolCard.js
@@ -11,7 +11,7 @@ export default function ToolCard({tool}){
             <img src={tool.url} alt={tool.name} className="toolpic" />
             </div>
             <p>{tool.description}</p>
-            <p>&#36;{tool.price}&#47;hour<span></span>  </p>
+            <p>&#36;{tool.price}&#47;hour<span></span> </p>
 
         <div className="ui two buttons">
             <Link to={`tools/${tool.id}`} className="ui basic button green"> Edit </Link>
