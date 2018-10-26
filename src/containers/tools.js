325b2d297ec2d3da984329116ffb4203f4a2d00e
illusioninlost <rchang9999@yahoo.com>
2018-10-26 06:48:38 -0700

    move code tools together

--- src/containers/tools.js
+++ src/containers/tools.js
@@ -3,9 +3,11 @@ import { connect } from 'react-redux';
 import { fetchTools } from '../actions/toolActions';
 // import Item from '../components/item'
 // import { Grid, Row, Col } from 'react-bootstrap';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 // import toolForm from './form'
-import ToolsList from './ToolsList';
+// import ToolsList from './ToolsList';
+
+
 
 
 
@@ -38,10 +40,44 @@ class Tools extends Component {
    
     render() {
 
+        const emptyMessage = (
+            <p> There are no tools to rent at this time. Please try again or post one. </p>
+        )
+        const resize = {fontSize:30, margin:0}
+        const description = {fontSize:20, color: '#8f8762'}
+        const price = {fontSize:25}
+        const counter = 0;
+       
+      
+    
+        const toolsList = (
+            <div className="ui three cards" >
+                {this.props.myTools.map(tool =>   
+                
+                <div className="ui card">
+                <div>
+                    <img src={tool.url} alt={tool.name} className="toolpic thumbnail" />
+                </div>
+                <p style={resize}>{tool.name}</p>
+                <p style={description}>{tool.description}</p>
+                <p style={price}>&#36;{tool.price}&#47;hour<span></span> </p>
+                <p><button onClick={this.handleClick}>{this.state.count}</button></p>
+
+                <div className="ui two buttons">
+                    <Link to={`tools/${tool.id}`} className="ui basic button green"> Edit </Link>
+                    <Link to={`tools/${tool.id}/delete`} className="ui basic button red"> Delete </Link>
+
+                </div>
+    
+            </div>)}
+            </div>
+        )
+
         return (
             <div>
-                <ToolsList tools={this.props.myTools} />
-                <button onClick={this.handleClick}>{this.state.count}</button>
+                 <div>
+                {toolsList === 0 ? emptyMessage : toolsList}
+            </div>
 
                 <Link to="/tools/new" className="add">Place New Tool AD</Link>
 
