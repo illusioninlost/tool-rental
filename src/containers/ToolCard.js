5427d956b4cd6c6cdbf8ce95f9bd2227454ee7b3
illusioninlost <rchang9999@yahoo.com>
2018-10-26 05:57:23 -0700

    merge toolcard

--- src/containers/ToolCard.js
+++ src/containers/ToolCard.js
@@ -1,36 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 
 
 export default function ToolCard({ tool }) {
-    const resize = {fontSize:30, margin:0}
-    const description = {fontSize:20, color: '#8f8762'}
-    const price = {fontSize:25}
-    const counter = 0;
-    
-    state = {
-        count: 0
-    }
+   
 
    function handleClick() {
     
       
    }
     return (
-        <div className="ui card">
-            <div>
-                <img src={tool.url} alt={tool.name} className="toolpic thumbnail" />
-            </div>
-            <p style={resize}>{tool.name}</p>
-            <p style={description}>{tool.description}</p>
-            <p style={price}>&#36;{tool.price}&#47;hour<span></span> </p>
-            <button onClick={handleClick}> Click me</button><p id={counter}>{this.state.counter}</p>
-            <div className="ui two buttons">
-                <Link to={`tools/${tool.id}`} className="ui basic button green"> Edit </Link>
-                <Link to={`tools/${tool.id}/delete`} className="ui basic button red"> Delete </Link>
-            </div>
-        </div>
+      
     )
 }
 
--- src/containers/ToolsList.js
+++ src/containers/ToolsList.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import ToolCard from './ToolCard';
+// import ToolCard from './ToolCard';
+import { Link } from 'react-router-dom';
+
 
 export default function ToolsList({ tools }) {
 
     const emptyMessage = (
         <p> There are no tools to rent at this time. Please try again or post one. </p>
     )
+    const resize = {fontSize:30, margin:0}
+    const description = {fontSize:20, color: '#8f8762'}
+    const price = {fontSize:25}
+    const counter = 0;
+   
+  
 
     const toolsList = (
-        <div className="ui three cards shrink">
-            {tools.map(tool => <ToolCard tool={tool} key={tool.id} />)}
+        <div className="ui three cards" >
+            {tools.map(tool =>   
+            
+            <div className="ui card">
+            <div>
+                <img src={tool.url} alt={tool.name} className="toolpic thumbnail" />
+            </div>
+            <p style={resize}>{tool.name}</p>
+            <p style={description}>{tool.description}</p>
+            <p style={price}>&#36;{tool.price}&#47;hour<span></span> </p>
+            <button> Click me</button>
+            <div className="ui two buttons">
+                <Link to={`tools/${tool.id}`} className="ui basic button green"> Edit </Link>
+                <Link to={`tools/${tool.id}/delete`} className="ui basic button red"> Delete </Link>
+            </div>
+
+        </div>)}
         </div>
     )
     return (
